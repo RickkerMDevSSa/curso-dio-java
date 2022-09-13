f80ee08fef94f13a83b176b1f54d7920a94a075e
Rickker <rickker.marques@hotmail.com>
2022-09-13 20:54:12 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 3;
+		System.out.println("hELLO" + (a+b));
+
+	}
+
+}
